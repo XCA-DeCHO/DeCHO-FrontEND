5b559c191a8f6d9684c37096d002c51fc1e953fb
Isaac Adewumi <ienioladewumi@gmail.com>
2022-01-19 14:02:34 +0100

    implemented native functions in Swift

---
+++ ios/RNAlgo.m
@@ -0,0 +1,16 @@
+//
+//  RNAlgo.m
+//  reDeCHO
+//
+//  Created by Isaac Adewumi on 19/01/2022.
+//
+
+#import <Foundation/Foundation.h>
+#import "React/RCTBridgeModule.h"
+
+@interface RCT_EXTERN_MODULE(RNAlgo, NSObject)
+
+RCT_EXTERN_METHOD(createAccount)
+RCT_EXTERN_METHOD(helloWorld)
+
+@end
---
+++ ios/reDeCHO-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  reDeCHO-Bridging-Header.h
+//  reDeCHO
+//
+//  Created by Isaac Adewumi on 19/01/2022.
+//
+
+#ifndef reDeCHO_Bridging_Header_h
+#define reDeCHO_Bridging_Header_h
+
+
+#endif /* reDeCHO_Bridging_Header_h */
+#import "React/RCTBridgeModule.h"
