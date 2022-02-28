90a269fe9a6839499025ca0b4721152298cf82b2
Joshua Praise Xekhai <joshpraise007@gmail.com>
2022-02-28 14:20:33 +0100

    Screens Refresh

--- src/globalComponents/ProgressBox/ProgressBox.js
+++ src/globalComponents/ProgressBox/ProgressBox.js
@@ -8,20 +8,24 @@ function ProgressBox (props) {
 
 
     return(
-      <VStack shadow={2} bg={colors.lightBlue} px={5} borderRadius={'md'} mt={5} py={2}>
+      <VStack px={5} borderRadius={'md'}
+       pb={2}>
         <Text
           m={1}
-          color={colors.black}
+          color={colors.lightgrey}
           fontSize={'18'}
-          fontFamily={'JosefinSans-Regular'}>
+          fontWeight={900}
+          fontFamily={'JosefinSans-Bold'}>
             {props.prefix}{Number(props.progress.toFixed(2)).toLocaleString()}
         </Text>
-        <Progress mb={1} value={100 * props.progress/props.goal} colorScheme={'emerald'}/>
+        <Progress mb={1} value={100 * props.progress/props.goal} colorScheme={'light'}/>
         <Text
           mb={1}
-          color={colors.black}
+          color={colors.lightgrey}
           fontSize={'18'}
-          fontFamily={'JosefinSans-Regular'}
+          fontWeight={900}
+
+          fontFamily={'JosefinSans-Bold'}
             alignSelf={'flex-end'}
         >
           {props.prefix}{Number(props.goal.toFixed(2)).toLocaleString()}
--- src/modes/anonymous/Approval/index.js
+++ src/modes/anonymous/Approval/index.js
@@ -1,4 +1,4 @@
-import React, {useRef} from 'react';
+import React, { useRef } from "react";
 
 import {
   Button,
@@ -12,65 +12,54 @@ import {
   VStack,
   FlatList,
   Spinner,
-} from 'native-base';
+  View,
+} from "native-base";
 import {
   TouchableOpacity,
   Dimensions,
   ImageBackground,
   Linking,
-} from 'react-native';
-import colors from '../../../utils/colors';
-import Clipboard from '@react-native-clipboard/clipboard';
-import NameBox from '../../../globalComponents/infoBox/NameBox';
-import ProgressBox from '../../../globalComponents/ProgressBox/ProgressBox';
-import QRCode from 'react-native-qrcode-svg';
+} from "react-native";
+import colors from "../../../utils/colors";
+import Clipboard from "@react-native-clipboard/clipboard";
+import NameBox from "../../../globalComponents/infoBox/NameBox";
+import ProgressBox from "../../../globalComponents/ProgressBox/ProgressBox";
+import QRCode from "react-native-qrcode-svg";
 
-function AnonymousApproval({navigation}) {
-  const settings = require('../../../../assets/images/options.png');
-  const close = require('../../../../assets/images/approval/close.png');
-  const info = require('../../../../assets/images/approval/info.png');
-  const dLogo = require('../../../../assets/images/approval/check.png');
-  const copy = require('../../../../assets/images/connectWallet/copy.png');
-  const logo = require('../../../../assets/images/logo/DechoLogomarkgradientlogomark.png');
-  const background = require('../../../../assets/images/bgImgs/blueWhite.jpg');
+function AnonymousApproval({ navigation }) {
+  const plus = require("../../../../assets/icons/+.png");
+  const donation1 = require("../../../../assets/icons/donation1.png");
+  const settings = require("../../../../assets/icons/settings-2.png");
+  const dLogo = require("../../../../assets/images/approval/check.png");
+  const copy = require("../../../../assets/images/connectWallet/copy.png");
+  const logo = require("../../../../assets/images/logo/DechoLogomarkgradientlogomark.png");
+  const background = require("../../../../assets/images/bgImgs/Artboard1.png");
   const toast = useToast();
 
-  const width = Dimensions.get('window').width;
+  const width = Dimensions.get("window").width;
 
   const flatListRef = useRef();
   const [showModal, setShowModal] = React.useState(false);
   const [approvals, setApprovals] = React.useState(null);
-  const [address, setAddress] = React.useState('');
-
-  //adding dynamic date greeting
-  let today = new Date();
-  let nowHour = today.getHours();
-  let greetingString;
-  if (nowHour < 12) {
-    greetingString = 'morning';
-  } else if (nowHour >= 12 && nowHour < 17) {
-    greetingString = 'afternoon';
-  } else {
-    greetingString = 'evening';
-  }
+  const [address, setAddress] = React.useState("");
 
   const onPressDisprove = (index) => {
     try {
-      flatListRef.current.scrollToIndex({animated: true, index: index + 1});
+      flatListRef.current.scrollToIndex({ animated: true, index: index + 1 });
     } catch (error) {
-      flatListRef.current.scrollToIndex({animated: true, index: index});
+      flatListRef.current.scrollToIndex({ animated: true, index: index });
       toast.show({
-        description: 'You have gotten to the end of the list',
+        description: "You have gotten to the end of the list",
       });
     }
   };
 
   if (!approvals) {
-    fetch('https://decho-staging.herokuapp.com/api/v1/causes')
+    fetch("https://decho-staging.herokuapp.com/api/v1/causes")
       .then((response) => response.json())
       .then((responseData) => {
         setApprovals(
-          responseData.data.filter((cause) => cause.status === 'pending'),
+          responseData.data.filter((cause) => cause.status === "pending")
         );
       })
       .catch((error) => console.log(error));
@@ -87,43 +76,67 @@ function AnonymousApproval({navigation}) {
           horizontal
           showsHorizontalScrollIndicator={false}
           pagingEnabled
-          renderItem={({item, index}) => {
+          renderItem={({ item, index }) => {
             return (
-              <VStack w={width} px={5} py={1}>
+              <VStack w={width} px={7} py={1}>
                 <NameBox
                   name={item.title}
                   slogan={item.short_description}
                   img={item.photo_url}
+                  balance={item.balance}
+                  goal={item.cause_approval.goal}
                 />
                 <HStack
-                  justifyContent={'center'}
-                  alignItems={'center'}
+                  justifyContent={"center"}
+                  alignItems={"center"}
                   space={5}
-                  mt={10}>
+                >
                   <TouchableOpacity
-                    onPress={() => {
-                      navigation.navigate('ViewInfo', {
-                        item,
-                      });
-                    }}>
-                    <Image source={info} alt={'info'} size={7} />
+                    style={{
+                      backgroundColor: colors.darkgrey,
+                      height: 60,
+                      width: 126,
+                      borderRadius: 20,
+                      alignItems: "center",
+                      justifyContent: "center",
+                      marginVertical: 20,
+                      flexDirection: "row",
+                    }}
+                    onPress={() => onPressDisprove(index)}
+                  >
+                    <Text
+                      color={colors.white}
+                      fontSize={18}
+                      fontFamily={"JosefinSans-Regular"}
+                    >
+                      Skip
+                    </Text>
                   </TouchableOpacity>
                   <TouchableOpacity
+                    style={{
+                      backgroundColor: colors.teal,
+                      height: 60,
+                      width: 126,
+                      borderRadius: 20,
+                      alignItems: "center",
+                      justifyContent: "center",
+                      marginVertical: 20,
+                      flexDirection: "row",
+                    }}
                     onPress={() => {
                       setAddress(item.decho_wallet.address);
                       setShowModal(true);
-                    }}>
-                    <Image source={dLogo} alt={'donate'} size={10} />
-                  </TouchableOpacity>
-                  <TouchableOpacity onPress={() => onPressDisprove(index)}>
-                    <Image source={close} alt={'disprove'} size={7} />
+                    }}
+                  >
+                    <Text
+                      color={colors.white}
+                      fontSize={18}
+                      fontFamily={"JosefinSans-Regular"}
+                    >
+                      Vote {'>'}
+                    </Text>
                   </TouchableOpacity>
                 </HStack>
-                <ProgressBox
-                  progress={item.balance}
-                  goal={item.cause_approval.goal}
-                  prefix={''}
-                />
               </VStack>
             );
           }}
@@ -133,136 +146,127 @@ function AnonymousApproval({navigation}) {
   }
 
   return (
-    <ImageBackground
-      source={background}
-      style={{
-        height: '100%',
-      }}>
-    <ScrollView >
-      <VStack w={'100%'} h={'100%'} pt={10} space={3}>
-        <TouchableOpacity
-          style={{
-            backgroundColor:colors.white,
-            borderRadius:150,
-            padding: 10,
-            width:50,
-            alignSelf:'flex-end',
-            margin:10,
-            elevation:5
-          }}
-          onPress={() => {
-            navigation.navigate('Options');
-          }}>
-          <Image
-            source={settings}
-            alt={'settings'}
-            size={7}
-            alignSelf={'flex-end'}
-            />
-          </TouchableOpacity>
-          <Text
-            mx={5}
-            color={colors.black}
-            fontSize={'24'}
-            fontWeight={'500'}
-            fontFamily={'JosefinSans-Bold'}>
-            Hi, Good {greetingString}.
-          </Text>
-          {/*  Commented out the search */}
-          {/*<Input mx={5} placeholder={'Search....'} />*/}
-          <Text fontSize={10} px={5}>
-            Swipe left to see more{'>>'}
-          </Text>
-          {checkLoading()}
-          <TouchableOpacity
-            onPress={() => {
-              navigation.push('AnonymousDonate');
-            }}>
+    <VStack w={"100%"} h={"100%"} backgroundColor={colors.black}>
+      <ImageBackground
+        source={background}
+        style={{
+          height: "100%",
+        }}
+      >
+        <ScrollView>
+          <VStack w={"100%"} h={"100%"} pt={45} space={3}>
             <Text
-              mx={10}
-              my={5}
-              color={colors.black}
-              fontSize={'12'}
-              fontFamily={'JosefinSans-Regular'}
-              alignSelf={'flex-end'}>
-              View approved projects{'>>'}
+              mx={7}
+              color={colors.white}
+              fontSize={"36"}
+              fontWeight={"900"}
+              fontFamily={"JosefinSans-Bold"}
+            >
+              Vote
             </Text>
-          </TouchableOpacity>
-        </VStack>
-        <Modal isOpen={showModal} onClose={() => setShowModal(false)}>
-          <Modal.Content w={'95%'}>
-            <Modal.CloseButton />
-            <Modal.Header>Vote</Modal.Header>
-            <Modal.Body>
-              <Text
-                my={2}
-                color={colors.black}
-                fontSize={'16'}
-                fontFamily={'JosefinSans-Regular'}>
-                Make your vote towards this project by sending CHOICE to this
-                address.
-                {'\n'}Your Choice will be refunded and rewarded!
-              </Text>
-              <HStack
-                alignSelf={'center'}
-                p={1}
-                m={2}
-                borderWidth={3}
-                borderColor={colors.grey}
-                borderRadius={'sm'}>
-                <QRCode
-                  value={address}
-                  size={250}
-                  logo={logo}
-                  logoBackgroundColor={colors.white}
-                  logoBorderRadius={800}
-                  color={colors.grey}
-                />
-              </HStack>
-              <Pressable
-                onPress={() => {
-                  Clipboard.setString(address);
-                  toast.show({
-                    description: 'Copied Address',
-                  });
-                }}
-                flexDirection={'row'}
-                background={colors.grey}
-                px={1}
-                py={5}
-                borderRadius={'md'}
-                justifyContent={'space-around'}>
+            
+            {checkLoading()}
+            <TouchableOpacity
+              onPress={() => {
+                navigation.push("AnonymousDonate");
+              }}
+            ></TouchableOpacity>
+          </VStack>
+          <Modal isOpen={showModal} onClose={() => setShowModal(false)}>
+            <Modal.Content w={"95%"}>
+              <Modal.CloseButton />
+              <Modal.Header>Vote</Modal.Header>
+              <Modal.Body>
                 <Text
+                  my={2}
                   color={colors.black}
-                  fontSize={'8'}
-                  fontFamily={'JosefinSans-Regular'}>
-                  {address}
+                  fontSize={"16"}
+                  fontFamily={"JosefinSans-Regular"}
+                >
+                  Make your vote towards this project by sending CHOICE to this
+                  address.
+                  {"\n"}Your Choice will be refunded and rewarded!
                 </Text>
-                <Image
-                  source={copy}
-                  alt="applause"
-                  h="3"
-                  w="3"
-                  alignSelf={'center'}
-                />
-              </Pressable>
-            </Modal.Body>
-            <Modal.Footer>
-              <Button
-                onPress={() => {
-                  Linking.openURL('algorand://main');
-                  setShowModal(false);
-                }}
-                bgColor="#0cb4cc"
-                colorScheme="teal">
-                Proceed
-              </Button>
-            </Modal.Footer>
-          </Modal.Content>
-        </Modal>
-      </ScrollView>
-    </ImageBackground>
+                <HStack
+                  alignSelf={"center"}
+                  p={1}
+                  m={2}
+                  borderWidth={3}
+                  borderColor={colors.grey}
+                  borderRadius={"sm"}
+                >
+                  <QRCode
+                    value={address}
+                    size={250}
+                    logo={logo}
+                    logoBackgroundColor={colors.white}
+                    logoBorderRadius={800}
+                    color={colors.grey}
+                  />
+                </HStack>
+                <Pressable
+                  onPress={() => {
+                    Clipboard.setString(address);
+                    toast.show({
+                      description: "Copied Address",
+                    });
+                  }}
+                  flexDirection={"row"}
+                  background={colors.grey}
+                  px={1}
+                  py={5}
+                  borderRadius={"md"}
+                  justifyContent={"space-around"}
+                >
+                  <Text
+                    color={colors.white}
+                    fontSize={"8"}
+                    fontFamily={"JosefinSans-Regular"}
+                  >
+                    {address}
+                  </Text>
+                  <Image
+                    source={copy}
+                    alt="applause"
+                    h="3"
+                    w="3"
+                    alignSelf={"center"}
+                  />
+                </Pressable>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button
+                  onPress={() => {
+                    Linking.openURL("algorand://main");
+                    setShowModal(false);
+                  }}
+                  bgColor={colors.teal}
+                  colorScheme="teal"
+                >
+                  Proceed
+                </Button>
+              </Modal.Footer>
+            </Modal.Content>
+          </Modal>
+        </ScrollView>
+      </ImageBackground>
+      <View style={{width:208,height:60,backgroundColor:colors.darkgrey, alignSelf:'center', borderRadius:99, position:'absolute', bottom:50, flexDirection:'row', justifyContent:'space-around', alignItems:'center'}}>
+      <TouchableOpacity>
+      <Image source={plus} width={6} height={6} alt={'Icon'}></Image></TouchableOpacity>
+      <TouchableOpacity style={{backgroundColor:colors.lightgrey, padding:10, borderRadius:99}}
+      onPress={
+        ()=>{navigation.navigate('AnonymousDonate')}
+      }>
+      <Image source={donation1} width={6} height={6} alt={'Icon'}></Image>
+      </TouchableOpacity><TouchableOpacity onPress={
+        ()=>{navigation.navigate('Options')}
+      }>
+      <Image source={settings} width={6} height={6} alt={'Icon'}></Image></TouchableOpacity>
+      </View>
+
+    </VStack>
   );
 }
 
 export default AnonymousApproval;
+
